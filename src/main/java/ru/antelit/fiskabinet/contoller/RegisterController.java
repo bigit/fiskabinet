7360d6c74b8fafe13a69ebed2707d5a8a9087a14
r.miller <rkmiller@antelit.ru>
2023-04-01 22:29:11 +0500

    17 Регистрация пользователй

--- src/main/java/ru/antelit/fiskabinet/contoller/RegisterController.java
+++ src/main/java/ru/antelit/fiskabinet/contoller/RegisterController.java
@@ -1,19 +1,40 @@
 package ru.antelit.fiskabinet.contoller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.antelit.fiskabinet.domain.UserInfo;
+import ru.antelit.fiskabinet.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 public class RegisterController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/register")
-    public String show() {
+    public String show(Model model) {
+        if (!model.containsAttribute("userInfo")) {
+            model.addAttribute("userInfo", new UserInfo());
+        }
         return "register";
     }
 
+    @SuppressWarnings("unused")
     @PostMapping("/register")
-    public String register() {
-        return "under_construct";
-    };
+    public String register(@Valid UserInfo userInfo, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        userInfo = userService.createUser(userInfo);
+
+        model.addAttribute("register", true);
+        return "login";
+    }
 }
--- src/main/java/ru/antelit/fiskabinet/domain/UserInfo.java
+++ src/main/java/ru/antelit/fiskabinet/domain/UserInfo.java
@@ -1,7 +1,8 @@
 package ru.antelit.fiskabinet.domain;
 
 import liquibase.repackaged.org.apache.commons.lang3.StringUtils;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,10 +10,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-@Data
 @Entity(name = "user")
 @Table(schema = "security", name = "users")
+@Getter
+@Setter
 public class UserInfo {
 
     @Id
@@ -20,27 +25,41 @@ public class UserInfo {
     private Integer id;
 
     @Column
+    @Size(min = 5, max = 24, message = "Логин должен быть не менее 5 символов")
     private String username;
 
     @Column
+    @Email (message = "Неверно указана почта")
+    @NotBlank(message = "Не указана почта")
     private String email;
 
     @Column(name = "phonenumber", columnDefinition = "bpchar", length = 11)
+    @NotBlank(message = "Не указан номер")
+    @Size(min = 11, max = 11, message = "Неверно указан номер")
     private String phone;
 
     @Column(name = "first_name")
+    @NotBlank(message = "Нe указано имя")
     private String firstName;
+
     @Column(name = "father_name")
     private String fatherName;
+
     @Column(name = "last_name")
+    @NotBlank(message = "Не указана фамилия")
     private String lastName;
+
+    @NotBlank(message = "Пароль не может быть пустым")
+    @Size(min = 8, message = "Минимум 8 символов")
     private String password;
+
     private boolean enabled;
 
     /**
      *
      * @return строку содержащую имя полностью (Иванов Иван Иванович)
      */
+    @SuppressWarnings("unused")
     public String getFullName() {
         return lastName + firstName + fatherName;
     }
