6a8d037d1a50ea0b9beebc80eaa958a960536573
r.miller <rkmiller@antelit.ru>
2023-06-19 15:52:29 +0500

    8 Добавить страницу для работы менеджера

--- src/main/java/ru/antelit/fiskabinet/service/repository/KkmRepository.java
+++ src/main/java/ru/antelit/fiskabinet/service/repository/KkmRepository.java
@@ -1,8 +1,10 @@
 package ru.antelit.fiskabinet.service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.antelit.fiskabinet.domain.Kkm;
+import ru.antelit.fiskabinet.domain.Organization;
 import ru.antelit.fiskabinet.domain.Tradepoint;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface KkmRepository extends JpaRepository<Kkm, Integer> {
 
    List<Kkm> getKkmByTradepoint(Tradepoint tradepoint);
 
+   @Query("select kkm from kkm where tradepoint.id in (" +
+           "select id from tradepoint where tradepoint.organization.id = #{organization.id})")
+   List<Kkm> getByOrganization(Organization organization);
 }
