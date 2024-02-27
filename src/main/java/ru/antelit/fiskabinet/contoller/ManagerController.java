74ca753be0a7aa20dcbde01ef0eaf84633ea0d82
r.miller <rkmiller@antelit.ru>
2024-02-27 16:01:48 +0500

    Merge pull request '14-interface' (#15) from 14-interface into main
    Reviewed-on: https://git.antelit.ru/Antel/fis-kabinet/pulls/15

--- src/main/java/ru/antelit/fiskabinet/contoller/ManagerController.java
+++ src/main/java/ru/antelit/fiskabinet/contoller/ManagerController.java
@@ -1,25 +1,20 @@
 package ru.antelit.fiskabinet.contoller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.antelit.fiskabinet.domain.Kkm;
 import ru.antelit.fiskabinet.domain.Organization;
 import ru.antelit.fiskabinet.service.BitrixService;
 import ru.antelit.fiskabinet.service.KkmService;
 import ru.antelit.fiskabinet.service.OrgService;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
-@Slf4j
 @Controller
 public class ManagerController {
 
